e14b128c555f469e46983e0b9633ad956e720e30
cbryanalfonso <cbryanalfonso@gmail.com>
2022-02-08 15:55:32 -0600

    New functionality

--- Screen/src/PersonajeIndividual.js
+++ Screen/src/PersonajeIndividual.js
@@ -40,7 +40,7 @@ export default function PersonajeIndividual({navigation, route}) {
             setVivo(false);
           } else if (value.status === 'unknown') {
             setDesconocido(true);
-          }
+          }//dsds
       });
   });
   return (
